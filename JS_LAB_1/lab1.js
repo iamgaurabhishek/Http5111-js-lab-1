4c46eba3ebcc0c83530b69bf0523b1889e5379ba
Abhishek Gaur <abhishekgaur1996@gmail.com>
2023-09-16 07:53:16 -0400

    Adding JS lab 1 folder

---
+++ JS_LAB_1/lab1.js
@@ -0,0 +1,11 @@
+//alert("Hello World!");
+
+// my age..
+var age = 27;
+var daysInYear = 365.25;
+var totalDaysOfAge = age * daysInYear;
+
+//alert(totalDaysOfAge);
+var a = "I am";
+var b = "days old....more or less";
+alert(a + " " + totalDaysOfAge + " " + b);
--- README.md
+++
@@ -1,7 +0,0 @@
-# js-lab-1
-_JS_ | First Lab of JS
-
-## This is just a start a long way to go with Git.
-
-### About me -
-My name is Abhishek Gaur | Web Developer
